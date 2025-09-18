25c53b534d82820ab0f1e3fc1880ff5d70ca54dc
Semyon <urliksemen2007@gmail.com>
2025-09-18 13:45:56 +0300

    Adding BigInteger && BigDecimal types

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Formatter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Test {
 
@@ -8,13 +10,13 @@ public class Test {
 		while (Math.abs(xDegrees) > 360) {
 			if (xDegrees < 0) {
 				xDegrees = -(Math.abs(xDegrees) - 360);
-			} 
-			else {
+			} else {
 				xDegrees -= 360;
 			}
 		}
 		return xDegrees;
 	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Formatter fmtX = new Formatter();
@@ -27,12 +29,17 @@ public class Test {
 		double e = Math.pow(10, -k);
 		xDegrees = xCount(xDegrees);
 		double xRadians = Math.toRadians(xDegrees);
-		
+
+		BigDecimal xRad = new BigDecimal(xRadians);
+		BigDecimal eBig = BigDecimal.ONE.divide(BigDecimal.TEN.pow(k));
+
 		Teylor row = new Teylor();
 		double sum = row.Teylor(xRadians, e);
+		BigDecimal sumBig = row.Teylor(xRad, eBig, k);
 
 		System.out.println("Taylor result: " + sum);
 		System.out.println("Math result: " + Math.sin(xRadians));
+		System.out.println("BigInteget && BigDecimal result: " + sumBig);
 
 		fmtX.format("Formatting x in octal (8) form: %o | in hexadecimal form: %x", xDegrees, xDegrees);
 		System.out.println(fmtX);
