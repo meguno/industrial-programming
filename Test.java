907959f4a1ac6fb259b7ca91234514c8eddb428d
Semyon <urliksemen2007@gmail.com>
2025-09-18 11:37:59 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,27 +2,32 @@ import java.util.Scanner;
 import java.util.Formatter;
 
 public class Test {
+
+	public static int xCount(int xDegrees) {
+
+		while (Math.abs(xDegrees) > 360) {
+			if (xDegrees < 0) {
+				xDegrees = -(Math.abs(xDegrees) - 360);
+			} 
+			else {
+				xDegrees -= 360;
+			}
+		}
+		return xDegrees;
+	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Formatter fmtX = new Formatter();
 		Formatter fmtK = new Formatter();
+
 		System.out.print("Enter x (degrees): ");
 		int xDegrees = scanner.nextInt();
 		System.out.print("Enter k: ");
 		int k = scanner.nextInt();
 		double e = Math.pow(10, -k);
-
-		while (Math.abs(xDegrees) > 360) {
-			if (xDegrees < 0) {
-				xDegrees = -(Math.abs(xDegrees) - 360);
-			} else {
-				xDegrees -= 360;
-			}
-
-		}
-
+		xDegrees = xCount(xDegrees);
 		double xRadians = Math.toRadians(xDegrees);
-
+		
 		Teylor row = new Teylor();
 		double sum = row.Teylor(xRadians, e);
 
